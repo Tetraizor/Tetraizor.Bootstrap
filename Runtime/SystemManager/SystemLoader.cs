316dab098abd93b749a5eef9c3d964a0aed4885a
Umuthan Özel <ozelumuthan@gmail.com>
2023-08-30 22:36:32 +0300

    Complete base features

--- Runtime/SystemManager/SystemLoader.cs
+++ Runtime/SystemManager/SystemLoader.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Tetraizor.Singleton;
 using UnityEngine;
 using Tetraizor.SystemManager.Base;
 using UnityEngine.Events;
@@ -7,18 +9,28 @@ using UnityEngine.SceneManagement;
 
 namespace Tetraizor.SystemManager
 {
-    public class SystemLoader : MonoBehaviour
+    public class SystemLoader : MonoSingleton<SystemLoader>
     {
+        #region Properties
+
+        [Header("System Properties")] 
         [SerializeField] private List<GameObject> _systems = new();
 
+        [Header("Common Scene Indices")]
         [SerializeField] private int _bootSceneIndex = 0;
         [SerializeField] private int _systemSceneIndex = 1;
-        [SerializeField] private int _mainMenuSceneIndex = 2;
 
-        public UnityEvent SystemLoadingCompleteEvent;
-        
-        private void Start()
+        private int _loadedSystemCount = 0;
+
+        public readonly UnityEvent SystemLoadingCompleteEvent = new UnityEvent();
+        public readonly UnityEvent<string> SendLoadStateMessageEvent = new UnityEvent<string>();
+        public readonly UnityEvent<IPersistentSystem, float> SystemLoadingStateChangeEvent = new UnityEvent<IPersistentSystem, float>();
+
+        #endregion
+
+        protected override void Init()
         {
+            base.Init();
             StartLoadingSystems();
         }
 
@@ -29,27 +41,71 @@ namespace Tetraizor.SystemManager
 
         private IEnumerator LoadSystemsAsync()
         {
-            Debug.Log("Starting to load systems...");
+            SendLoadStateMessageEvent?.Invoke("System loading started.");
+
+            // Load empty scene to insert systems into.
+            AsyncOperation systemSceneLoadingOperation =
+                SceneManager.LoadSceneAsync(_systemSceneIndex, LoadSceneMode.Additive);
+            systemSceneLoadingOperation.allowSceneActivation = true;
+
+            while (!systemSceneLoadingOperation.isDone)
+            {
+                yield return null;
+            }
+
+            // Make system scene active to create System prefabs in.
             SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(_systemSceneIndex));
 
+            SendLoadStateMessageEvent?.Invoke("Systems scene created and set active.");
+            
+            // Instantiate and load each system.
             foreach (GameObject systemPrefab in _systems)
             {
-                GameObject systemInstance = Instantiate(systemPrefab, Vector3.zero, Quaternion.identity);
-                IPersistentSystem persistentSystem = systemInstance.GetComponent<IPersistentSystem>();
+                IPersistentSystem persistentSystem = LoadSystem(systemPrefab);
 
                 if (persistentSystem == null)
                 {
-                    Debug.LogWarning($"{systemInstance.name} does not implement the interface 'IPersistentScene'. " +
-                                     $"This system will be ignored.");
-                    
+                    SendLoadStateMessageEvent?.Invoke($"{systemPrefab.name} does not implement the interface 'IPersistentScene'. " +
+                                                      $"This system will be ignored.");
                     continue;
                 }
 
                 yield return persistentSystem.LoadSystem();
-                Debug.Log($"{persistentSystem.GetName()} finished loading.");
+
+                _loadedSystemCount++;
+
+                SendLoadStateMessageEvent?.Invoke($"{persistentSystem.GetName()} finished loading.");
             }
 
-            Debug.Log("Finished loading all the systems. Loading menu scene.");
+            // Reset active scene back to boot scene.
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(_bootSceneIndex));
+
+            print(SceneManager.GetActiveScene().name);
+
+            yield return new WaitForEndOfFrame();
+
+            SystemLoadingCompleteEvent?.Invoke();
+
+            SendLoadStateMessageEvent?.Invoke("Finished loading all the systems. Loading menu scene.");
+        }
+
+        public void UpdateLoadingState(IPersistentSystem system, float loadingPercentage)
+        {
+            float totalLoadingPercentage =
+                ((float)_loadedSystemCount / _systems.Count) + loadingPercentage / _systems.Count;
+
+            SystemLoadingStateChangeEvent?.Invoke(system, totalLoadingPercentage);
+        }
+
+        private IPersistentSystem LoadSystem(GameObject systemPrefab)
+        {
+            GameObject systemInstance = Instantiate(systemPrefab, Vector3.zero, Quaternion.identity);
+
+            systemInstance.name = systemPrefab.name;
+
+            IPersistentSystem persistentSystem = systemInstance.GetComponent<IPersistentSystem>();
+
+            return persistentSystem;
         }
     }
 }
